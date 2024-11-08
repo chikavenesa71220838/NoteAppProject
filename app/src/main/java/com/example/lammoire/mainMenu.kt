7338318993245d5aad2fc41c43593a85858c05a9
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2024-11-08 13:51:39 +0700

    coba push

---
+++ app/src/main/java/com/example/lammoire/mainMenu.kt
@@ -0,0 +1,13 @@
+package com.example.lammoire
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class mainMenu : Fragment(R.layout.fragment_main_menu) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)   }
+    }
