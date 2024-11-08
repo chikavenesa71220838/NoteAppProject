3f52182ff5415da79ac8fbbd129d4ca8b604dc8e
71220935_Dwiyan Bagus Prakosa <112855980+rapunzeru@users.noreply.github.com>
2024-11-08 12:58:24 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/lammoire/loginpage.kt
@@ -0,0 +1,13 @@
+package com.example.lammoire
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class loginpage : Fragment(R.layout.fragment_loginpage) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
