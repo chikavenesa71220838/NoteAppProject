2da35a9cd8daa3a68ac97b4784c993181bc1aa12
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2024-11-12 23:07:14 +0700

    firebase configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.gms.google-services")
+//    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
@@ -47,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-analytics")
 }
