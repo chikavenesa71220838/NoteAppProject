a634b919c6f57ee35d0577295d2274010eabef44
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2024-11-29 11:50:25 +0700

    implementasi fingerprint

--- app/src/main/java/com/example/lammoire/MainActivity.kt
+++ app/src/main/java/com/example/lammoire/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.lammoire
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var auth: FirebaseAuth
-
-    // In your MainActivity.kt
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Set the activity layout
 
         val sharedPreferences = getSharedPreferences("user_session", android.content.Context.MODE_PRIVATE)
         val logged = sharedPreferences.getBoolean("is_logged_in", false)
 
+
+        val navController = findNavController(R.id.nav_host_fragment)
         if (logged) {
-            setContentView(R.layout.fragment_startafterlogin)
+            navController.navigate(R.id.startafterlogin)
         } else {
-            setContentView(R.layout.activity_main)
+            navController.navigate(R.id.startMenu)
         }
+
     }
 }
